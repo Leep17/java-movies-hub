24041ffc0317669d52ade626e04485938c8f9a80
Max Solovov <m_solovov1@mail.ru>
2026-02-08 21:04:13 +0300

    Спринт 9 v.1

--- src/main/ru/practicum/moviehub/http/MoviesServer.java
+++ src/main/ru/practicum/moviehub/http/MoviesServer.java
@@ -1,5 +1,39 @@
 package ru.practicum.moviehub.http;
 
-public class MoviesServer {
+import com.sun.net.httpserver.HttpServer;
+import ru.practicum.moviehub.store.MoviesStore;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class MoviesServer {
+    private final HttpServer server;
+    private final MoviesStore store;
+
+
+    public MoviesServer() {
+        try {
+
+            server = HttpServer.create(new InetSocketAddress(8080), 0);
+            store = new MoviesStore();
+            server.createContext("/movies", new MoviesHandler(store));
+
+        } catch (IOException e) {
+            throw new RuntimeException("Не удалось создать HTTP-сервер", e);
+        }
+    }
+
+    public void start() {
+        server.start();
+        System.out.println("Сервер запущен");
+    }
+
+    public void stop() {
+        server.stop(0);
+        System.out.println("Сервер остановлен");
+    }
+
+    public void reset() {
+        store.deleteAll();
+    }
 }
